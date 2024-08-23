7f85e5b4d2f8e68f0c983ed7037a4b1e04180ae9
dembezum <dembezuuma@gmail.com>
2024-08-23 14:52:45 +0200

    update bashls

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -23,7 +23,13 @@
         terraformls.enable = true; # Enable Terraform language server
         ansiblels.enable = true; # Enable Ansible language server
         pyright.enable = true; # Enable Python language server
-        bashls.enable = true; # Enable Bash language server
+        bashls = {
+          enable = true;
+          autostart = true;
+          rootDir = "null";
+          
+
+        };
         cssls.enable = true; # Enable CSS language server
         marksman.enable = true; # Enable Markdown language server
         zls.enable = true; # Enable Zig language server

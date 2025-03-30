98e239e6c4b336c8af941bf8d04109b0766881ac
dembezum <dembezuuma@gmail.com>
2025-03-30 11:42:24 +0200

    formatting

--- config/plugins/treesitter/default.nix
+++ config/plugins/treesitter/default.nix
@@ -13,11 +13,11 @@
       settings = { max_lines = 2; };
     };
   };
-	keymaps = [{
-		mode = "n";
-		key = "<leader>tc";
-		action = "<cmd>TSContextToggle<CR>";
-		options.desc = "Toggle treesitter context";
-	}];
+  keymaps = [{
+    mode = "n";
+    key = "<leader>tc";
+    action = "<cmd>TSContextToggle<CR>";
+    options.desc = "Toggle treesitter context";
+  }];
 
 }

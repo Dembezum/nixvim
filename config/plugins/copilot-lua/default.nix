b45ad9d68818dac33c1544fdff852c61a2015b4f
dembezum <dembezuuma@gmail.com>
2025-03-30 12:49:32 +0200

    fix copilot-lua

--- config/plugins/copilot-lua/default.nix
+++ config/plugins/copilot-lua/default.nix
@@ -1,24 +1,21 @@
 {
   # copilot-lua configuration
-
   plugins = {
     copilot-lua = {
-      enable = true;
-      suggestion.enabled = false;
-	      panel.enabled = false;
-      filetypes = {
-        markdown = true;
-        c = true;
-        yaml = true;
-        zig = true;
-        nix = true;
-        "." = false;
+      settings = {
+        suggestion.enabled = false;
+        panel.enabled = false;
+        filetypes = {
+          markdown = true;
+          c = true;
+          yaml = true;
+          zig = true;
+          nix = true;
+          "." = false;
+        };
       };
-    };
-    copilot-chat = {
       enable = true;
-
     };
+    copilot-chat = { enable = true; };
   };
-
 }

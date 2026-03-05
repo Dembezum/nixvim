4ef9cd461a14e627fe2cae5371e00daa4aecb76f
dembezum <dembezuuma@gmail.com>
2026-03-05 13:30:23 +0100

    update

--- config/plugins/screenkey/default.nix
+++ config/plugins/screenkey/default.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }:
 {
-  plugins.screenkey-nvim = {
-    enable = true;
-    extraConfigLua = ''
-      require("screenkey").setup({
-        win_opts = {
-          relative = "editor",
-          anchor = "SE",
-          width = 40,
-          height = 1,
-          row = -2,
-          col = -2,
-          border = "rounded",
-          style = "minimal",
-        },
-        show_in_insert = true,
-        show_in_cmd = true,
-      })
-    '';
-  };
+  extraPlugins = [
+    pkgs.vimPlugins.screenkey-nvim
+  ];
+
+  extraConfigLua = ''
+    require("screenkey").setup({
+      win_opts = {
+        relative = "editor",
+        anchor = "SE",
+        width = 40,
+        height = 1,
+        row = -2,
+        col = -2,
+        border = "rounded",
+        style = "minimal",
+      },
+      show_in_insert = true,
+      show_in_cmd = true,
+    })
+  '';
 }

24b30fdc9947ef3da20c14ab2b87a277892aa7ec
dembezum <dembezuuma@gmail.com>
2026-03-05 13:28:24 +0100

    update

--- config/plugins/screenkey/default.nix
+++ config/plugins/screenkey/default.nix
@@ -1,29 +1,22 @@
 { pkgs, ... }:
 {
-  extraPlugins = [
-    (pkgs.vimUtils.buildVimPluginFromGitHub {
-      name = "screenkey.nvim";
-      owner = "NStefan002";
-      repo = "screenkey.nvim";
-      rev = "main";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
-    })
-  ];
-
-  extraConfigLua = ''
-    require("screenkey").setup({
-      win_opts = {
-        relative = "editor",
-        anchor = "SE",
-        width = 40,
-        height = 1,
-        row = -2,
-        col = -2,
-        border = "rounded",
-        style = "minimal",
-      },
-      show_in_insert = true,
-      show_in_cmd = true,
-    })
-  '';
+  plugins.screenkey-nvim = {
+    enable = true;
+    extraConfigLua = ''
+      require("screenkey").setup({
+        win_opts = {
+          relative = "editor",
+          anchor = "SE",
+          width = 40,
+          height = 1,
+          row = -2,
+          col = -2,
+          border = "rounded",
+          style = "minimal",
+        },
+        show_in_insert = true,
+        show_in_cmd = true,
+      })
+    '';
+  };
 }
